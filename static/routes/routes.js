feb094dbf4028bf7a7957df9275655b9285eda60
Mark Huang <mah049@ucsd.edu>
2016-11-02 22:43:23 -0700

    test

--- static/routes/routes.js
+++ static/routes/routes.js
@@ -1,10 +1,6 @@
 // Get all of our friend data
 var data = require('../../data.json');
 
-//Function that is called when the page is loaded
-window.onload=function(){
-	alert(hi);	
-}
 
 exports.view = function(req, res){
 };
