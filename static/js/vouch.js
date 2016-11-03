d59cd958a4b162c0d07e3eb00399c496a62980fd
Mark Huang <mah049@ucsd.edu>
2016-11-03 15:15:05 -0700

    fresh start

--- static/js/vouch.js
+++ static/js/vouch.js
@@ -1,4 +1,9 @@
+var	vouch_funcs = require('../routes/routes.js');
 
+window.onload=function() { 
+   alert ('hello world');
+   vouch_funcs.view();
+};
 
 $("#checkAll").click(function () {
     $(".check").prop('checked', $(this).prop('checked'));
--- static/routes/routes.js
+++ static/routes/routes.js
@@ -1,6 +1,6 @@
 // Get all of our friend data
 var data = require('../../data.json');
 
-
 exports.view = function(req, res){
+  res.render('index.handlebars', {'name': Mark});
 };
