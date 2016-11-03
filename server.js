eb55044285cce48055ac57d787dc9ae9898ae278
Mark Huang <mah049@ucsd.edu>
2016-11-03 00:43:43 -0700

    handlebar stuff

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var	routes = require('./static/routes/routes.js');
 // Express is a web framework for node.js
 // that makes nontrivial applications easier to build
 var express = require('express');
+var handlebars = require('express3-handlebars')
+var path = require('path');
+//static webpages
 
 // Create the server instance
 var app = express();
@@ -13,12 +16,27 @@ var app = express();
 // Print logs to the console and compress pages we send
 app.use(express.logger());
 app.use(express.compress());
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.cookieParser('Intro HCI secret key'));
+app.use(express.session());
+app.use(app.router);
+
 //Serves static files
 // Return all pages in the /static directory
 // whenever they are requested at '/'
 // e.g., http://localhost:3000/index.html
 // maps to /static/index.html on this machine
 app.use(express.static(__dirname + '/static'));
+
+
+ app.get('/', function(req, res) {
+     res.sendfile(__dirname+'/static/index.handlebars');
+ });
 
 //app.get('/', function(req, res) {
 //    res.sendFile(express.static(path.join(__dirname + '/static/addNew.html')));
