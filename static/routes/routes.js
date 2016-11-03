7e647553b047e184d07ad5a86eac30e19a7d77d3
Mark Huang <mah049@ucsd.edu>
2016-11-03 16:40:57 -0700

    handlebars for index

--- static/routes/routes.js
+++ static/routes/routes.js
@@ -2,9 +2,9 @@
 var data = require('../../data.json');
 
 exports.getDeals = function(req,res){
-	
+
 }
 
 exports.view = function(req, res){
-  res.render('index.handlebars', {'name': 'Mark'});
+  res.render('index.handlebars', {'deal' : [{'name':'Mc','deal':'20dollars'},{'name':'Mc','deal':'20dollars'}]});
 };
